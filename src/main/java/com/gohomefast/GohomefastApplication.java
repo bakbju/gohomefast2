83ad056fb5bef20393bb0db5ac7619261605123c
bakbju <bakbju@gmail.com>
2018-03-07 22:14:46 +0900

    first commit

---
+++ src/main/java/com/gohomefast/GohomefastApplication.java
@@ -0,0 +1,12 @@
+package com.gohomefast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GohomefastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GohomefastApplication.class, args);
+	}
+}
---
+++ src/main/java/com/gohomefast/security/SecurityConfiguration.java
@@ -0,0 +1,14 @@
+package com.gohomefast.security;
+
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/**");
+    }
+}
